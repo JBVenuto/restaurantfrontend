6ecd6bd33176c7d4f4156ba149f78629b1c4270e
Joseph Venuto <jbvenuto@gmail.com>
2019-02-21 16:14:25 -0500

    Copied the view folder from restaurants into this project

---
+++ src/components/AllRestaurants.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const AllRestaurants = (props) => (
+    
+    <div className="card">
+        <div className="card-body">
+            {props.name}
+        </div>
+    </div> 
+)
+
+export default AllRestaurants;
