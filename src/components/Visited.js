01f113b5c67d440c8de4536635b7c976bb30d019
Joseph Venuto <jbvenuto@gmail.com>
2019-05-07 10:42:20 -0400

    Changed the displayed message with the user's rating

--- src/components/Visited.js
+++ src/components/Visited.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 
-const Visited = (props) => (
-    <div className="card">
-        <div className="card-body">
-           <span
-                id="cardVisitedText"
-           > 
-                {props.name}
-           </span>
-           <span className="returnChoice">Would you return? {props.visitAgain}</span>
-        </div>
-    </div> 
-);
+const Visited = (props) => {
+    if (props.visitAgain) {
+        return (
+            <div className="card">
+                <div className="card-body">
+                <span
+                        id="cardVisitedText"
+                > 
+                        {props.name}
+                </span>
+                <span className="returnChoice">You liked this place</span>
+                </div>
+            </div> 
+        )
+    }
+    else {
+        return (
+            <div className="card">
+                <div className="card-body">
+                <span
+                        id="cardVisitedText"
+                > 
+                        {props.name}
+                </span>
+                <span className="returnChoice">You didn't liked this place</span>
+                </div>
+            </div>
+        )
+    }
+};
 
 export default Visited;
